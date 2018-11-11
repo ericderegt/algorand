f709755f817003b5cd4ba091558c68bbb4bc0346
nickgreenquist <nickgreenquist@gmail.com>
2018-11-11 17:01:20 -0500

    modified blockchain code to be simple proof of stake lottery for appending new blocks

--- simple-blockchain/README.md
+++ simple-blockchain/README.md
@@ -6,7 +6,8 @@ Credit: https://medium.com/@mycoralhealth/code-your-own-blockchain-in-less-than-
 2. `go get github.com/gorilla/mux`
 3. `go get github.com/joho/godotenv`
 4. `go run main.go`
-5. Open a new terminal
+5. Open a few new terminals
 6. Make sure netcat is installed correctly - https://github.com/rsanchez-wsu/jfiles/wiki/Windows-10-Telnet-&-NetCat
 7. `nc localhost 9000`
-8. Enter new commands to create new blocks
+8. Enter node's starting stake
+8. Enter new amounts to enter into lottery and try to get block appended
--- simple-blockchain/main.go
+++ simple-blockchain/main.go
@@ -13,6 +13,8 @@ import (
 	"strconv"
 	"time"
 	"sync"
+	"fmt"
+	"math/rand"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/joho/godotenv"
@@ -32,23 +34,33 @@ type Block struct {
 	Amount    int
 	Hash      string
 	PrevHash  string
+	Validator string
 }
 
 // Global functions
 var Blockchain []Block
-var bcServer chan[]Block // handles incoming concurrent Blocks
+var tempBlocks []Block
+var candidateBlocks = make(chan Block)
+var announcements = make(chan string)
+
 var mutex = &sync.Mutex{}
 
+var validators = make(map[string]int)
+
 // Blockchain Code
-func calculateHash(block Block) string {
-	record := string(block.Index) + block.Timestamp + block.Receiver + block.Payer + string(block.Amount) + block.PrevHash
+func calculateHash(s string) string {
 	h := sha256.New()
-	h.Write([]byte(record))
+	h.Write([]byte(s))
 	hashed := h.Sum(nil)
 	return hex.EncodeToString(hashed)
 }
 
-func generateBlock(oldBlock Block, Receiver string, Payer string, Amount int) (Block, error) {
+func calculateBlockHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + block.Receiver + block.Payer + string(block.Amount) + block.PrevHash
+	return calculateHash(record)
+}
+
+func generateBlock(oldBlock Block, Receiver string, Payer string, Amount int, address string) (Block, error) {
 
 	var newBlock Block
 
@@ -60,13 +72,14 @@ func generateBlock(oldBlock Block, Receiver string, Payer string, Amount int) (B
 	newBlock.Payer = Payer
 	newBlock.Amount = Amount
 	newBlock.PrevHash = oldBlock.Hash
-	newBlock.Hash = calculateHash(newBlock)
+	newBlock.Hash = calculateBlockHash(newBlock)
+	newBlock.Validator = address
 
 	return newBlock, nil
 }
 
 func isBlockValid(newBlock, oldBlock Block) bool {
-	if oldBlock.Index+1 != newBlock.Index || oldBlock.Hash != newBlock.PrevHash || calculateHash(newBlock) != newBlock.Hash {
+	if oldBlock.Index+1 != newBlock.Index || oldBlock.Hash != newBlock.PrevHash || calculateBlockHash(newBlock) != newBlock.Hash {
 		return false
 	}
 	return true
@@ -99,32 +112,32 @@ func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload i
 	w.Write(response)
 }
 
-func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
-	var m Message
-
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&m); err != nil {
-		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
-		return
-	}
-	defer r.Body.Close()
-
-	newBlock, err := generateBlock(Blockchain[len(Blockchain)-1], m.Receiver, m.Payer, m.Amount)
-	if err != nil {
-		respondWithJSON(w, r, http.StatusInternalServerError, m)
-		return
-	}
-	if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
-		newBlockchain := append(Blockchain, newBlock)
-		replaceChain(newBlockchain)
-		spew.Dump(Blockchain)
-	}
-
-	respondWithJSON(w, r, http.StatusCreated, newBlock)
-}
-
 func handleConn(conn net.Conn) {
 	defer conn.Close()
+
+	go func() {
+		for {
+			msg := <-announcements
+			io.WriteString(conn, msg)
+		}
+	}()
+	var address string //validator
+
+	// users input their init stake
+	io.WriteString(conn, "Enter token balance:")
+	scanBalance := bufio.NewScanner(conn)
+	for scanBalance.Scan() {
+		balance, err := strconv.Atoi(scanBalance.Text())
+		if err != nil {
+			log.Printf("%v not a number: %v", scanBalance.Text(), err)
+			return
+		}
+		t := time.Now()
+		address = calculateHash(t.String())
+		validators[address] = balance
+		fmt.Println(validators)
+		break
+	}
 
 	io.WriteString(conn, "Enter a new amount:")
 
@@ -136,44 +149,97 @@ func handleConn(conn net.Conn) {
 			amount, err := strconv.Atoi(scanner.Text())
 			if err != nil {
 				log.Printf("%v not a number: %v", scanner.Text(), err)
+				delete(validators, address) // bad input revokes your status as validator
 				continue
 			}
-			newBlock, err := generateBlock(Blockchain[len(Blockchain)-1], "Nick", "Eric", amount)
+			mutex.Lock()
+			oldLastIndex := Blockchain[len(Blockchain) - 1]
+			mutex.Unlock()
+
+			newBlock, err := generateBlock(oldLastIndex, "Nick", "Eric", amount, address)
 			if err != nil {
 				log.Println(err)
 				continue
 			}
-			if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
-				newBlockchain := append(Blockchain, newBlock)
-				replaceChain(newBlockchain)
+			if isBlockValid(newBlock, oldLastIndex) {
+				candidateBlocks <- newBlock
 			}
-
-			bcServer <- Blockchain
 			io.WriteString(conn, "\nEnter a new amount:")
 		}
 	}()
 
 	// re-broadcast the blockchain every 10 seconds
-	go func() {
-		for {
-			time.Sleep(10 * time.Second)
+	for {
+		time.Sleep(10 * time.Second)
 
-			// put a lock on Blockchain datastructure when adding to it
-			mutex.Lock()
-			output, err := json.Marshal(Blockchain)
-			if err != nil {
-				log.Fatal(err)
+		// put a lock on Blockchain datastructure when adding to it
+		mutex.Lock()
+		output, err := json.Marshal(Blockchain)
+		mutex.Unlock()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		io.WriteString(conn, string(output) + "\n")
+	}
+}
+
+// randomly picks from validators, weighted by stake
+func pickWinner() {
+	time.Sleep(10 * time.Second)
+
+	mutex.Lock()
+	temp := tempBlocks 
+	mutex.Unlock()
+
+	lotteryPool := []string{}
+	if len(temp) > 0 {
+		//modified proof of stake algorithm
+	OUTER:
+		for _, block := range temp {
+			// if already in lottery pool, skip
+			for _, node := range lotteryPool {
+				if block.Validator == node {
+					continue OUTER
+				}
 			}
+
+			mutex.Lock()
+			setValidators := validators
 			mutex.Unlock()
 
-			io.WriteString(conn, string(output))
+			k, ok := setValidators[block.Validator]
+			if ok {
+				for i := 0; i < k; i++ {
+					lotteryPool = append(lotteryPool, block.Validator)
+				}
+			}
 		}
-	}()
 
-	// pretty print the Blockchain
-	for _ = range bcServer {
-		spew.Dump(Blockchain)
+		// randomly pick from the lottery pool
+		s := rand.NewSource(time.Now().Unix())
+		r := rand.New(s)
+		winner := lotteryPool[r.Intn(len(lotteryPool))]
+
+		// add winner's block to blockchain and broadcast
+		for _, block := range temp {
+			if block.Validator == winner {
+				mutex.Lock()
+				Blockchain = append(Blockchain, block)
+				mutex.Unlock()
+
+				for _ = range validators {
+					announcements <- "\nWinning Validator: " + winner + "\n"
+				}
+				break
+			}
+		}
 	}
+
+	mutex.Lock()
+	tempBlocks = []Block{}
+	mutex.Unlock()
 }
 
 func main() {
@@ -182,20 +248,36 @@ func main() {
 		log.Fatal(err)
 	}
 
-	bcServer = make(chan []Block)
-
 	// set up Genesis block
 	t := time.Now()
-	genesisBlock := Block{0, t.String(), "", "", 0, "", "",}
+	genesisBlock := Block{}
+	genesisBlock = Block{0, t.String(), "", "", 0, calculateBlockHash(genesisBlock), "", ""}
 	spew.Dump(genesisBlock)
 	Blockchain = append(Blockchain, genesisBlock)
 
-	// start TCP and serve it
-	server, err := net.Listen("tcp", ":"+os.Getenv("ADDR"))
+	httpPort := os.Getenv("PORT")
+
+	// start TCP and serve TCP server
+	server, err := net.Listen("tcp", ":"+httpPort)
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("HTTP Server Listening on port :", httpPort)
 	defer server.Close()
+
+	go func() {
+		for candidate := range candidateBlocks {
+			mutex.Lock()
+			tempBlocks = append(tempBlocks, candidate)
+			mutex.Unlock()
+		}
+	}()
+
+	go func() {
+		for {
+			pickWinner()
+		}
+	}()
 
 	// create a new connection each time we receive a request
 	for {
